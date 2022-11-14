212306f5ac1972bccd433694cf3e0349eb1f3764
Will MacKenzie <34073318+whmacken@users.noreply.github.com>
2022-11-14 13:24:16 -0800

    new commit

---
+++ R/harmonize_factors.R
@@ -0,0 +1,17 @@
+#' Harmonize factor levels
+#'
+#' @param target_vs_pred A data frame  with target and .pred_class columns
+#' @return List with optimal downsample and smote
+#'
+#' @author
+#' @examples
+#'
+
+harmonize_factors <- function(target_vs_pred){
+  targ.lev <- levels(target_vs_pred$target)
+  pred.lev <- levels(target_vs_pred$.pred_class)
+  levs <- c(targ.lev, pred.lev) %>% unique()
+  target_vs_pred$target <- factor(target_vs_pred$target, levels = levs)
+  target_vs_pred$.pred_class <- factor(target_vs_pred$.pred_class, levels = levs)
+  return(target_vs_pred)
+}
