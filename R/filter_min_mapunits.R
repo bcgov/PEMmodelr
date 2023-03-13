d7fb43b4f0308416f123796d684f00a47ba084fe
gcperk <genevieve.c.perkins@gmail.com>
2023-03-12 17:03:11 -0700

    add report function, helper fuctions and report themeing.

---
+++ R/filter_min_mapunits.R
@@ -0,0 +1,43 @@
+#' Filter out mapunits based on minimum number of records
+#'
+#' @param tpts training data points set
+#' @param min_no minimum no of mapunit points (based on Origin position )
+#' @import dplyr
+#' @import magrittr
+#' @return dataframe with the filtered mapunits removed
+#' @export
+#'
+#' @examples
+#' filter_min_mapunits(tpts, 20)
+#'
+filter_min_mapunits <- function(tpts, min_no){
+
+  #min_no =100
+ # tpts = alldat
+
+  if("position" %in% names(tpts)) {
+    otpts <- tpts %>%
+      filter(position == "Orig")
+  } else {
+    otpts <- tpts
+  }
+
+  MU_count <- otpts %>%
+    dplyr::count(mapunit1)
+
+  todrop <- MU_count %>%
+    dplyr::filter(n < min_no)
+
+  print("dropping the following mapunits:")
+  print(todrop$mapunit1)
+
+  tokeep <- MU_count %>%
+      dplyr::filter(n >= min_no)
+
+  mdat <- tpts %>%
+    dplyr::filter(mapunit1 %in% tokeep$mapunit1)%>%
+    droplevels()
+
+  return(mdat)
+
+}
