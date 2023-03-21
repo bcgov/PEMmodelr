0afb4ed7d27dd700386d2227818b93e8c587b7d6
gcperk <genevieve.c.perkins@gmail.com>
2023-03-21 10:34:12 -0700

    testing

--- R/balance_opt.R
+++ R/balance_opt.R
@@ -25,18 +25,19 @@
 #' diag(covmat) <- 0
 #' trDat <- trDat[,c(rep(TRUE,5),!apply(covmat, 2, function(x) any(abs(x) > 0.9, na.rm = TRUE))),with = FALSE]
 #'
-#rawDat <- st_read("./PEM_dev/s1_clean_neighbours_allatts.gpkg")
-#rawDat <- as.data.table(st_drop_geometry(rawDat))
-#rawDat <- rawDat[grep("ESSFmc_",mapunit1),]
-#rawDat[,c("CellNum","transect_id","data_type","transition","observer","comments","ID.1") := NULL]
-#clnDat <- rawDat[,c(rep(T,6),colSums(is.na(rawDat[,-c(1:6), with = F])) == 0), with = FALSE]
-#covmat <- cor(clnDat[,-(1:6), with = FALSE])
-#covmat[upper.tri(covmat)] <- 0
-#diag(covmat) <- 0
-#clnDat <- clnDat[,c(rep(TRUE,6),!apply(covmat, 2, function(x) any(abs(x) > 0.9, na.rm = TRUE))),with = FALSE]
-#fMat <- fread("./fuzzy_matrix_basic_updated.csv")
-#train_data <- clnDat
-#train_data <- fread("TestPredict.csv")
+# rawDat <- st_read("./PEM_dev/s1_clean_neighbours_allatts.gpkg")
+# rawDat <- st_read("D:/PEM_DATA/PEMmodelr/CanyonCreek/10_clean_inputs/30_trainingdata/s1_transect_all_pts_att.gpkg")
+# rawDat <- as.data.table(st_drop_geometry(rawDat))
+# rawDat <- rawDat[grep("ESSFmc_",mapunit1),]
+# rawDat[,c("CellNum","transect_id","data_type","transition","observer","comments","ID.1") := NULL]
+# clnDat <- rawDat[,c(rep(T,6),colSums(is.na(rawDat[,-c(1:6), with = F])) == 0), with = FALSE]
+# covmat <- cor(clnDat[,-(1:6), with = FALSE])
+# covmat[upper.tri(covmat)] <- 0
+# diag(covmat) <- 0
+# clnDat <- clnDat[,c(rep(TRUE,6),!apply(covmat, 2, function(x) any(abs(x) > 0.9, na.rm = TRUE))),with = FALSE]
+# fMat <- fread("./fuzzy_matrix_basic_updated.csv")
+# train_data <- clnDat
+# train_data <- fread("TestPredict.csv")
 #
 #opt_res <- optimise_balance(train_data = clnDat, fuzz_matrix = fMat, num_slice = 2)
 #  getBestPars(opt_res)
@@ -61,24 +62,32 @@
 
 optimise_balance <- function(train_data, fuzz_matrix, num_slice = 2, n_iters = 4, mtry = 14,min_n = 7, use.neighbours = TRUE,
                              acc_mets = c("spat_paf_theta.5","aspat_paf_theta.5","spat_paf_theta0")){
- # # # data lines
+ # # # # data lines
+ #  train_data <- trDat %>%
+ #    filter(slice != 1)#%>%
+ #    #dplyr::select(-bgc_cat,-transect_id)
+ #
  #  train_data = as.data.table(trDat)
+ #  train_data <-  train_data[grep("ICHmc2_",mapunit1),]
+ #  train_data <- droplevels(train_data)
+ #
  #  fuzz_matrix = fmat
- #  num_slice = 5
- #  n_iters = 4
- #  use.neighbours = TRUE
- #  acc_mets = c("spat_paf_theta.5","aspat_paf_theta.5","spat_paf_theta0")
- #  mtry = mtry
- #  min_n = min_n
- #  # end data lines
+ #  num_slice = 4
+ #   n_iters = 4
+ #   use.neighbours = TRUE
+ #   acc_mets = c("spat_paf_theta.5","aspat_paf_theta.5","spat_paf_theta0")
+ #   mtry = 4 #mtry
+ #   min_n = 7# min_n
+ #   # end data lines
 
   ref_dat <- copy(train_data)
   ref_dat[,mapunit1 := as.factor(mapunit1)]
   print("Training raw data models...")
 
-  ref_acc <- foreach(k = 1:num_slice, .combine = rbind) %do% {
+  #ref_acc <- foreach(k = 1:num_slice, .combine = rbind) %do% {
+  ref_acc <- foreach(k = 2:num_slice, .combine = rbind) %do% {
     ref_train <- ref_dat[slice != k & position == "Orig",]
-    ref_train[,c("id","tid","mapunit2", "position","slice") := NULL]
+    ref_train[,c("id","tid","mapunit2", "position","slice","transect_id","bgc_cat") := NULL]
     low_units <- ref_train[,.(NumUnit = .N), by = .(mapunit1)][NumUnit < 10,]
     ref_train <- ref_train[!mapunit1 %in% low_units$mapunit1,]
 
@@ -113,18 +122,22 @@ optimise_balance <- function(train_data, fuzz_matrix, num_slice = 2, n_iters = 4
   ref_acc_fn <- mean(ref_acc_all)
 
   ##function to train model
-  train_mod <- function(downsample, smote, trDat = train_data, ref_metrix = ref_acc_fn, UN = use.neighbours){
-    sresults <- foreach(k = 1:num_slice, .combine = rbind) %do% {
-      BGC_train <- trDat[slice != k & position == "Orig",]
+  train_mod <- function(downsample, smote, ttrDat = train_data, ref_metrix = ref_acc_fn, UN = use.neighbours){
+    #sresults <- foreach(k = 1:num_slice, .combine = rbind) %do% {
+
+    sresults <- foreach(k = 2:num_slice, .combine = rbind) %do% {
+      #k = 2
+      #ttrDat = train_data
+      BGC_train <- ttrDat[slice != k & position == "Orig",]
       MU_count <- BGC_train[,.(NumUnit = .N), by = .(mapunit1)][NumUnit < 10,]
       BGC_train <- BGC_train[!mapunit1 %in% MU_count$mapunit1,]
       BGC_train[,mapunit1 := as.factor(mapunit1)]
-      BGC_train[,c("ID","tid","mapunit2", "position","slice") := NULL]
+      BGC_train[,c("id","tid","mapunit2", "position","slice","transect_id","bgc_cat") := NULL]
 
       if (UN) {
-        BGC_test <- trDat[slice == k & !mapunit1 %in% MU_count$mapunit1,]
+        BGC_test <- ttrDat[slice == k & !mapunit1 %in% MU_count$mapunit1,]
       }else{
-        BGC_test <- trDat[slice == k & !mapunit1 %in% MU_count$mapunit1 & Position == "Orig",]
+        BGC_test <- ttrDat[slice == k & !mapunit1 %in% MU_count$mapunit1 & Position == "Orig",]
       }
 
       null_recipe <-  recipe(mapunit1 ~ ., data = BGC_train) %>%
@@ -132,13 +145,13 @@ optimise_balance <- function(train_data, fuzz_matrix, num_slice = 2, n_iters = 4
         step_smote(mapunit1, over_ratio = smote, neighbors = 2, skip = TRUE) %>%
         prep()
 
-      train_data <- bake(null_recipe,new_data = NULL)
+      train_data <- bake(null_recipe, new_data = NULL)
       #train_data$mapunit1 <- as.factor(as.character(train_data$mapunit1))
-      PEM_rf1 <- ranger(mapunit1 ~ ., data = train_data, mtry = 26,
-                        num.trees = 151, min.node.size = 6, importance = "permutation")
+      PEM_rf1 <- ranger(mapunit1 ~ ., data = train_data, mtry = mtry,
+                        num.trees = 151, min.node.size = min_n, importance = "permutation")
 
       test.pred <-  predict(PEM_rf1, BGC_test)
-      test.pred <- cbind(BGC_test[,.(id = ID, mapunit1, mapunit2, slice)],
+      test.pred <- cbind(BGC_test[,.(id, mapunit1, mapunit2, slice)],
                          .pred_class = as.character(test.pred$predictions))
       test.pred <- test.pred[!is.na(test.pred$.pred_class),]
 
