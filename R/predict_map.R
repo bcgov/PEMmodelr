063c2271a3a1ef9ce7621ecfa9a36b99b3ee3013
gcperk <genevieve.c.perkins@gmail.com>
2023-04-12 16:50:01 -0700

    placeholder for map built

--- R/predict_map.R
+++ R/predict_map.R
@@ -5,14 +5,22 @@
 predict_map <- function(model, cov, tile_size, out_dir){
 
 
+  model = fmodel
+
+  covs =
+
+
+   rf_fit <-extract_fit_parsnip(fmodel)
+
+  # get predictor set:
+  rf_features <-
+    rf_fit %>%
+    extract_fit_engine() %>%
+    caret::predictors()  #<- the caret funciton
 
 
 
-
-
-
-
-
+  PEMprepr::mosaic_covariates
 
 
 }
