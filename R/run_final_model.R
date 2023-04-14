58b0046bea0d735732027a0587b92bbd223e776c
gcperk <genevieve.c.perkins@gmail.com>
2023-04-14 11:42:46 -0700

    add packages

--- R/run_final_model.R
+++ R/run_final_model.R
@@ -7,6 +7,8 @@
 #' @param ds_ratio numeric
 #' @param sm_ratio numeric
 #' @return parsnip model object
+#' @import parsnip
+#' @import recipes
 #' @export
 #' @examples
 #' run_final_model(train_data,fuzz_matrix, mtry = 14, min_n = 7, ds_ratio = NULL,sm_ratio = NULL)
@@ -29,9 +31,9 @@ run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, s
   ref_dat <- ref_dat[complete.cases(ref_dat[, 2:length(ref_dat)]), ]
 
   #set up model params
-  randf_spec <- rand_forest(mtry = mtry, min_n = min_n, trees = 200) %>%
-    set_mode("classification") %>%
-    set_engine("ranger", importance = "permutation", verbose = FALSE)
+  randf_spec <- parsnip::rand_forest(mtry = mtry, min_n = min_n, trees = 200) %>%
+    parsnip::set_mode("classification") %>%
+    parsnip::set_engine("ranger", importance = "permutation", verbose = FALSE)
 
 
   # set up downsample and smote options
@@ -40,15 +42,15 @@ run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, s
 
     print("no downsample or smoting")
 
-    best_recipe <-  recipe(mapunit1 ~ ., data = ref_dat)
+    best_recipe <-  recipes::recipe(mapunit1 ~ ., data = ref_dat)
 
   }
   if(is.null(ds_ratio) & !is.null(sm_ratio)){
 
     print("applying smoting")
 
-    best_recipe <-  recipe(mapunit1 ~ ., data = ref_dat) %>%
-      step_smote(mapunit1, over_ratio = sm_ratio , neighbors = 10, skip = TRUE)
+    best_recipe <-  recipes::recipe(mapunit1 ~ ., data = ref_dat) %>%
+      themis::step_smote(mapunit1, over_ratio = sm_ratio , neighbors = 10, skip = TRUE)
 
   }
 
@@ -56,30 +58,30 @@ run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, s
 
     print("applying downsample")
 
-    best_recipe <-  recipe(mapunit1 ~ ., data = ref_dat) %>%
-      step_downsample(mapunit1, under_ratio = ds_ratio)
+    best_recipe <-  recipes::recipe(mapunit1 ~ ., data = ref_dat) %>%
+      themis::step_downsample(mapunit1, under_ratio = ds_ratio)
 
   }
   if(!is.null(ds_ratio) & !is.null(sm_ratio)){
 
     print("applying downsample and smoting")
 
-    best_recipe <-  recipe(mapunit1 ~ ., data = ref_dat) %>%
-      step_downsample(mapunit1, under_ratio = ds_ratio) %>%
-      step_smote(mapunit1, over_ratio = sm_ratio , neighbors = 10, skip = TRUE)
+    best_recipe <-  recipes::recipe(mapunit1 ~ ., data = ref_dat) %>%
+      themis::step_downsample(mapunit1, under_ratio = ds_ratio) %>%
+      themis::step_smote(mapunit1, over_ratio = sm_ratio , neighbors = 10, skip = TRUE)
 
   }
 
   # set up workflow
 
-  pem_workflow <- workflow() %>%
-    add_recipe(best_recipe) %>%
-    add_model(randf_spec)
+  pem_workflow <- workflows::workflow() %>%
+    workflows::add_recipe(best_recipe) %>%
+    workflows::add_model(randf_spec)
 
   # run model
   print("running model")
 
-  PEM_rf <- fit(pem_workflow, ref_dat)
+  PEM_rf <- parsnip::fit(pem_workflow, ref_dat)
 
   return(PEM_rf)
 
