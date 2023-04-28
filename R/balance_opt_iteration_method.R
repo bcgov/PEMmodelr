e14d4d00c73f85170a00669b3fc2d56dfb1a3992
gcperk <genevieve.c.perkins@gmail.com>
2023-04-27 17:49:29 -0700

    chnage data tabel back to dplyr... needs more work.

--- R/balance_opt_iteration_method.R
+++ R/balance_opt_iteration_method.R
@@ -28,16 +28,16 @@ balance_optimisation_iteration <- function(train_data = train_data,
                                  #smote_iterations = smote_iterations,
                                  #use.neighbours = TRUE) {
 
-  # # # testing lines
-  # train_data =train_data
-  # mtry = mtry
-  # min_n = min_n
-  # ds_iterations <- 0.5
-  # smote_iterations <- NA #c(0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9)
-  # fuzz_matrix <- fmat
-  # out_dir <- fid$model_inputs0310[2]
-  # #
+  # # # # testing lines
 
+    train_data = tdat
+    fuzz_matrix = fmat
+    ds_iterations = 30 #c(10,20,30,40,50,60,70,80,90),
+    smote_iterations = NA #c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7,0.8, 0.9),
+    mtry = mtry
+    min_n = min_n
+    use.neighbours = TRUE
+    out_dir = outDir
   # create a subfolder to store all balance outputs:
 
   dir.create(file.path(out_dir, "balance"))
@@ -156,13 +156,136 @@ balance_optimisation_iteration <- function(train_data = train_data,
      print("downsample only")
 
      for(d in ds_iterations){
-       #d = ds_iterations[1]
+       d = ds_iterations[1]
        print(d)
        # set up the parameters for balancing
        downsample_ratio = d  # (0 - 100, Null = 1)
        balance_name = paste0("ds_",downsample_ratio)
 
        # training set - train only on pure calls
+       ref_dat <- train_data %>% #[,1:20] %>%
+         dplyr::mutate(mapunit1 = as.factor(mapunit1),
+                       slice = as.factor(slice))
+       print("Training raw data models...")
+
+       munits <- unique(ref_dat$mapunit1)
+       nf_mapunits <- grep(munits, pattern = "_\\d", value = TRUE, invert = TRUE)
+
+       slices <- unique(ref_dat$slice) %>% droplevels()
+
+       #
+       ref_acc <- foreach::foreach(k = levels(slices),.combine = rbind) %do% {
+
+         k = levels(slices)[1]
+         label = paste(d, k, sep = "-")
+         print(label)
+
+         ref_train <- ref_dat %>%
+           dplyr::filter(!slice %in% k) %>%
+           filter(is.na(mapunit2)) %>%
+           filter(position == "Orig") %>%
+           dplyr::select(-slice, -target2, -tid, -transect_id)
+           # train only on pure calls
+
+         BGC_train <- BGC_train %>%
+           dplyr::select(-slice, -target2, -tid, -transect_id) %>%
+           droplevels()
+
+
+
+
+
+
+         ref_train <- ref_dat[slice != k & position == "Orig",]
+         ref_train[,c("id","mapunit2", "position","slice","transect_id","bgc_cat") := NULL]
+         low_units <- ref_train[,.(NumUnit = .N), by = .(mapunit1)][NumUnit < 10,]
+         ref_train <- ref_train[!mapunit1 %in% low_units$mapunit1,]
+
+         if (use.neighbours) {
+           ref_test <- ref_dat[slice == k & !mapunit1 %in% low_units$mapunit1,]
+         }else{
+           ref_test <- ref_dat[slice == k & !mapunit1 %in% low_units$mapunit1 & position == "Orig",]
+         }
+
+         null_recipe <-  recipe(mapunit1 ~ ., data = ref_train) %>%
+           update_role(tid, new_role = "id variable") %>%
+           step_downsample(mapunit1, under_ratio = downsample_ratio)
+
+         randf_spec <- rand_forest(mtry = mtry, min_n = min_n, trees = 151) %>%
+           set_mode("classification") %>%
+           set_engine("ranger", importance = "permutation", verbose = FALSE)
+
+         pem_workflow <- workflows::workflow() %>%
+           workflows::add_recipe(null_recipe) %>%
+           workflows::add_model(randf_spec)
+
+         #######################################################
+         possibleError <- tryCatch(
+           fit(pem_workflow, ref_train),
+           error=function(e) e
+         )
+         if(!inherits(possibleError, "error")){
+           #REAL WORK
+           ref_mod <- fit(pem_workflow, ref_train)
+
+           final_fit <- tune::extract_fit_parsnip(PEM_rf1)
+
+           oob  <- round(PEM_rf1$fit$fit$fit$prediction.error, 3)
+
+           preds <- predict(ref_mod, ref_test)
+           pred_all <- cbind(ref_test[,.(id, mapunit1, mapunit2, slice)],
+                             .pred_class = preds$.pred_class)
+
+           pred_all <- pred_all %>% mutate(mapunit1 = as.character(mapunit1),
+                                           mapunit2 = as.character(mapunit2),
+                                           .pred_class = as.character(.pred_class))
+
+           pred_all <- pred_all %>%
+             dplyr::mutate(mapunit1 = ifelse(mapunit1  %in% nf_mapunits, "nonfor", mapunit1 ),
+                           mapunit2 = ifelse(mapunit2 %in% nf_mapunits, "nonfor", mapunit2) ,
+                           .pred_class = ifelse(.pred_class  %in% nf_mapunits, "nonfor", .pred_class ))
+
+           pred_all$mapunit1 <- as.factor(pred_all$mapunit1)
+           pred_all$mapunit2 <- as.factor(pred_all$mapunit2)
+           #pred_all$.pred_class <- factor(pred_all$.pred_class,
+           #                                levels = levels(pred_all$mapunit1))
+
+           print(paste0("generating accuracy metrics for slice:",k))
+
+           acc <- acc_metrics(pred_all, fuzzmatrx = fuzz_matrix)%>%
+             dplyr::mutate(slice = k)
+
+         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
        ref_dat <- copy(train_data)
        ref_dat[,mapunit1 := as.factor(mapunit1)]
        ref_dat[,slice := as.factor(slice)]
@@ -175,7 +298,7 @@ balance_optimisation_iteration <- function(train_data = train_data,
 
        ref_acc <- foreach::foreach(k = levels(slices),.combine = rbind) %do% {
 
-        # k = levels(slices)[5]
+         k = levels(slices)[2]
          label = paste(d, k, sep = "-")
          print(label)
 
@@ -240,7 +363,7 @@ balance_optimisation_iteration <- function(train_data = train_data,
 
          }
 
-       } # end of slice loop (downsample only )
+       } # end of slice loop (downsample only)
 
        # extract results from sresults
 
