030c170cf462b919e86958562913ae5c047930ea
gcperk <genevieve.c.perkins@gmail.com>
2023-04-28 08:55:23 -0700

    add error check

--- R/harmonize_factors.R
+++ R/harmonize_factors.R
@@ -8,8 +8,8 @@
 #' @export
 
 harmonize_factors <- function(target_vs_pred){
-  targ.lev <- levels(target_vs_pred$mapunit1)
-  pred.lev <- levels(target_vs_pred$.pred_class)
+  targ.lev <- levels(as.factor(target_vs_pred$mapunit1))
+  pred.lev <- levels(as.factor(target_vs_pred$.pred_class))
   levs <- c(targ.lev, pred.lev) %>% unique()
   target_vs_pred$mapunit1 <- factor(target_vs_pred$mapunit1, levels = levs)
   target_vs_pred$.pred_class <- factor(target_vs_pred$.pred_class, levels = levs)
