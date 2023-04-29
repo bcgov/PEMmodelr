58aae930bc57c92baeb0989ed91f4ed0072ec578
gcperk <genevieve.c.perkins@gmail.com>
2023-04-29 13:24:08 -0700

    comment out temp script

--- R/tile_temp.R
+++ R/tile_temp.R
@@ -1,13 +1,13 @@
-
-
-
-create_tiles <- function(trast, tile_size, out_dir ){
-
-  trast <- rtemplate
-  tile_size = 500
-  out_dir = tile_dir
-
-  makeTiles(trast, tile_size, filename = file.path(out_dir, "tile_.tif"), extend = T, na.rm = FALSE)
-
-
-}
+#
+#
+#
+# create_tiles <- function(trast, tile_size, out_dir ){
+#
+#   trast <- rtemplate
+#   tile_size = 500
+#   out_dir = tile_dir
+#
+#   makeTiles(trast, tile_size, filename = file.path(out_dir, "tile_.tif"), extend = T, na.rm = FALSE)
+#
+#
+# }
