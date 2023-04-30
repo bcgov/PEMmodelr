cf6dc1cbdfa6e802904e3f7e891415c2b07d65b1
gcperk <genevieve.c.perkins@gmail.com>
2023-04-30 09:24:25 -0700

    testing

--- R/run_final_model.R
+++ R/run_final_model.R
@@ -17,36 +17,43 @@
 run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, sm_ratio = NULL){
 
     #
-    # ref_dat= final_data
-    # fuzz_matrix = fmat
-    # mtry = 14 #mtry
-    # min_n = 7 #min_n
-    # ds_ratio = NULL
-    # sm_ratio = NULL
+     ref_dat = final_data
+     fuzz_matrix = fmat
+     mtry = mtry
+     min_n = min_n
+     ds_ratio = ds_ratio
+     sm_ratio = sm_ratio
+
 
   # prep data
-  ref_dat <- as.data.table(ref_dat)
-  ref_dat[,mapunit1 := as.factor(mapunit1)]
-  low_units <- ref_dat[,.(NumUnit = .N), by = .(mapunit1)][NumUnit < 10,]
-  ref_dat <- ref_dat[!mapunit1 %in% low_units$mapunit1,]
-  ref_dat <- ref_dat[complete.cases(ref_dat[, 2:length(ref_dat)]), ]
+     ref_dat <- ref_dat %>% mutate(mapunit1 = as.factor(mapunit1))
 
-  #set up model params
-  randf_spec <- parsnip::rand_forest(mtry = mtry, min_n = min_n, trees = 200) %>%
-    parsnip::set_mode("classification") %>%
-    parsnip::set_engine("ranger", importance = "permutation", verbose = FALSE)
+     MU_count <- ref_dat %>% dplyr::count(mapunit1) %>% filter(n > 10)
+
+     ref_dat <- ref_dat %>% filter(mapunit1 %in% MU_count$mapunit1)  %>%
+       droplevels()
+
+     munits <- unique(ref_dat$mapunit1)
+     nf_mapunits <- grep(munits, pattern = "_\\d", value = TRUE, invert = TRUE)
+
+     ref_dat <- ref_dat[complete.cases(ref_dat[, 2:length(ref_dat)]), ]
+
+    #set up model params
+    randf_spec <- parsnip::rand_forest(mtry = mtry, min_n = min_n, trees = 200) %>%
+     parsnip::set_mode("classification") %>%
+     parsnip::set_engine("ranger", importance = "permutation", verbose = FALSE)
 
 
   # set up downsample and smote options
 
-  if(is.null(ds_ratio) & is.null(sm_ratio)){
+  if(is.na(ds_ratio) & is.na(sm_ratio)){
 
     print("no downsample or smoting")
 
     best_recipe <-  recipes::recipe(mapunit1 ~ ., data = ref_dat)
 
   }
-  if(is.null(ds_ratio) & !is.null(sm_ratio)){
+    if(is.na(ds_ratio) & !is.na(sm_ratio)){
 
     print("applying smoting")
 
@@ -55,7 +62,7 @@ run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, s
 
   }
 
-  if(!is.null(ds_ratio) & is.null(sm_ratio)){
+  if(!is.na(ds_ratio) & is.na(sm_ratio)){
 
     print("applying downsample")
 
@@ -63,7 +70,7 @@ run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, s
       themis::step_downsample(mapunit1, under_ratio = ds_ratio)
 
   }
-  if(!is.null(ds_ratio) & !is.null(sm_ratio)){
+  if(!is.na(ds_ratio) & !is.na(sm_ratio)){
 
     print("applying downsample and smoting")
 
@@ -80,7 +87,7 @@ run_final_model <- function(ref_dat,fuzz_matrix, mtry, min_n, ds_ratio = NULL, s
     workflows::add_model(randf_spec)
 
   # run model
-  print("running model")
+  print("running final PEM model")
 
   PEM_rf <- parsnip::fit(pem_workflow, ref_dat)
 
