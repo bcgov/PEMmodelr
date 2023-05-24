e48f7f6a2435e816992e3e182b219660a1a0ca86
gcperk <genevieve.c.perkins@gmail.com>
2023-05-24 08:04:29 -0700

    spilt theta calcs into two functions

--- R/select_theta_threshold.R
+++ R/select_theta_threshold.R
@@ -1,4 +1,4 @@
-#' select_theta_threshold
+#' generate_theta_threshold
 #'
 #' @param allthetas output of the generate theta metric function
 #' @param metric text of which metric you wish to use, default = "paf"
@@ -10,25 +10,34 @@
 #' @export
 #'
 #' @examples
-#' select_theta_threshold(alltheta, "paf")
-
-select_theta_threshold <- function(allthetas,metric = "paf"){
+#' generate_theta_threshold(alltheta, "paf")
+generate_theta_threshold <- function(allthetas){
 
   #allthetas = acc_out
   #metric = "paf"
 
-  mnames = paste0(metric,"_theta")
+  metrics = as.factor(c("p", "pa", "paf"))
 
-  noi <-  names(allthetas)[str_detect(names(allthetas),mnames)]
+  ref_acc <- foreach::foreach(k = levels(metrics),.combine = rbind) %do% {
 
-  acc <- allthetas  %>%
-    dplyr::select(slice, theta, any_of(noi)) %>%
-    dplyr::distinct()
+    k = levels(metrics)[1]
+
+    mnames = paste0(k,"_theta")
+
+    noi <-  names(allthetas)[str_detect(names(allthetas),mnames)]
+
+    acc <- allthetas  %>%
+     dplyr::select(slice, theta, any_of(noi)) %>%
+     dplyr::distinct()
   #write.csv(acc, file.path(datafolder, "compiled_theta_results.csv")
 
-  acc2 <- acc  %>%
-    tidyr::pivot_longer(cols = where(is.numeric), names_to = "accuracy_type", values_to = "value") %>%
-    dplyr::distinct()
+    acc2 <- acc  %>%
+      tidyr::pivot_longer(cols = where(is.numeric), names_to = "accuracy_type", values_to = "value") %>%
+      dplyr::distinct()
+
+
+    acc2 <- acc2 %>% select(-slice)
+
 
   acc <- acc2 %>%
     dplyr::mutate(type = case_when(
@@ -38,7 +47,32 @@ select_theta_threshold <- function(allthetas,metric = "paf"){
       stringr::str_detect(accuracy_type, "theta0") ~ 0,
       stringr::str_detect(accuracy_type, "theta.5") ~ NA,
       stringr::str_detect(accuracy_type, "theta1") ~ 1)) %>%
-    dplyr::mutate(theta_final = ifelse(is.na(theta_base), theta, theta_base))
+    dplyr::mutate(theta_final = ifelse(is.na(theta_base), theta, theta_base))%>%
+    dplyr::select(-theta_base)
+
+   bal_out <- acc %>%
+     group_by(type, theta_final) %>%
+    #group_by(type, theta_final)%>%
+    summarise(mean = mean(value),
+              q25 = quantile(value, prob = 0.25),
+              q75 = quantile(value, prob = 0.75)) %>%
+    mutate(above_thresh = ifelse(q25 <= 0.65, F, T))
+
+
+}
+   overall_acc <- ggplot(aes(y = value, x = theta_final), data = acc ) +
+     geom_boxplot() +
+     scale_fill_brewer(type = "qual") +
+     facet_wrap(~type, scales = "free_x")+
+     geom_hline(yintercept = 0.65,linetype ="dashed", color = "black") +
+     theme_pem_facet() +
+     # scale_fill_manual(values=c("grey90", "grey75", "grey50", "grey35","grey10"))+
+     theme(axis.text.x = element_text(angle = 90, hjust = 1), legend.position="none") +
+     xlab("Metric") + ylab("Accuracy") +
+     ylim(0, 1)
+
+   overall_acc
+
 
 
   acc_out <- acc %>% dplyr::select(type, slice, value, theta_final)
